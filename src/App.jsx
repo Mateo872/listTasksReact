d5dcc23303dd625be4c7a9a9dca60b80182f8494
Mateo Bellini <bellinimateo872@gmail.com>
2023-05-17 10:39:57 -0300

    Maquetado de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <h1>Titulo</h1>
+      <Container className="my-5 main_page">
+        <h1 className="display-4 text-center mb-0">Titulo</h1>
+        <hr />
+        {/* Formulario */}
+      </Container>
+      <footer className="bg-dark text-light text-center py-4">
+        <p className="mb-0">&copy; Todos los derechos reservados</p>
+      </footer>
     </>
   );
 }
